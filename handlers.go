3d869861db13c2e10ba510bc9dff7bd2641541c3
thealamu <vaguemail369@gmail.com>
2022-02-07 00:18:04 +0100

    feat: implement as-a-meter handler

---
+++ handlers.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"sync"
+	"time"
+)
+
+const (
+	RequestsCap = 5 //number at which we start dropping requests
+	LeakRate    = 1 * time.Second
+)
+
+//handleAsAMeter implements Leaky Bucket as a Meter.
+//See https://en.wikipedia.org/wiki/Leaky_bucket#As_a_meter
+func handleAsAMeter() http.HandlerFunc {
+	var (
+		meter int
+		mu    sync.Mutex
+	)
+
+	//constant leak rate
+	ticker := time.NewTicker(LeakRate)
+	go func(ticker *time.Ticker) {
+		for range ticker.C {
+			mu.Lock()
+			if meter == 0 {
+				continue
+			}
+			meter--
+			mu.Unlock()
+		}
+	}(ticker)
+
+	return func(w http.ResponseWriter, r *http.Request) {
+		//attend to request if meter won't overflow, otherwise, drop request
+		mu.Lock()
+		if meter == RequestsCap {
+			w.WriteHeader(http.StatusTooManyRequests)
+			mu.Unlock()
+			fmt.Fprintln(w, "Request dropped, overflowing.......")
+			return
+		}
+		meter++
+		mu.Unlock()
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprintln(w, "Pong")
+	}
+}
